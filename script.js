31c04148ee7d0e9e124748ed671de099f0e9f779
danjpal16 <525dpalagano@frhsd.com>
2024-02-01 14:50:49 -0500

    project is in progress

---
+++ script.js
@@ -0,0 +1,66 @@
+function year1742(){ 
+    document.getElementById('changeImage').src = '' 
+    document.getElementById('response-text').innerText='On April 13, 1742, George Frideric Handels iconic oratorio "Messiah" made its debut at the Music Hall in Dublin. The composition, with its timeless "Hallelujah" chorus, has since become a celebrated masterpiece in classical music, symbolizing the spirit of the Easter season.' 
+    document.getElementById('response-date').innerText= 'April 13, 1742' } 
+
+
+
+
+
+
+
+function orignalYear(){ 
+    document.getElementById('changeImage').src = ''
+    document.getElementById('response-text').innerText='On a remarkable day in history, I made a grand entrance into the world, bringing joy and excitement to those lucky enough to witness the momentous occasion. My birthdate is now etched into the tapestry of time, a day that marked the beginning of a unique journey filled with infinite possibilities. As the world welcomed this new soul, the air seemed to shimmer with anticipation, and the stars above may have shared a special twinkle, as if celebrating the arrival of someone extraordinary. Since that memorable day, I have embarked on a personal odyssey, navigating the intricate tapestry of life with grace and resilience, leaving an indelible mark on the hearts of those fortunate enough to share in the joy of their existence.' 
+    document.getElementById('response-date').innerText= 'April 13, 2007' } 
+
+
+
+
+
+
+
+function year1743(){ 
+    document.getElementById('changeImage').src = '' 
+    document.getElementById('response-text').innerText='Founding Father and third President of the United States, Thomas Jefferson, was born on April 13, 1743. A key architect of American democracy, Jefferson played a pivotal role in drafting the Declaration of Independence and later served as President from 1801 to 1809.' 
+    document.getElementById('response-date').innerText= 'April 13, 1743' } 
+    
+    
+    
+    
+    
+    
+    
+function year1943(){ 
+    document.getElementById('changeImage').src = ''
+    document.getElementById('response-text').innerText='The Warsaw Ghetto Uprising, a courageous act of resistance against Nazi oppression during World War II, began on April 13, 1943. Jewish inhabitants of the Warsaw Ghetto in Poland fought valiantly against overwhelming odds, symbolizing the indomitable human spirit in the face of tyranny.'
+    document.getElementById('response-date').innerText= 'April 13, 1943' } 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+function year1997(){ 
+    document.getElementById('changeImage').src = '' 
+    document.getElementById('response-text').innerText='On April 13, 1997, golf prodigy Tiger Woods made history by winning his first major golf tournament, the Masters Tournament at Augusta National Golf Club. This victory marked the beginning of an illustrious career, with Woods becoming one of the greatest golfers in the sports history.' 
+    document.getElementById('response-date').innerText= 'April 13, 1997' } 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+function year1970(){ 
+    document.getElementById('changeImage').src = '' 
+    document.getElementById('response-text').innerText='A critical event occurred on April 13, 1970, during the Apollo 13 mission to the moon. An oxygen tank exploded, jeopardizing the safety of the astronauts. Through the ingenuity and teamwork of NASAs Mission Control and the crew, including Commander James Lovell, the mission ultimately became a triumph of crisis management as they safely returned to Earth.' 
+    document.getElementById('response-date').innerText= 'April 13, 1970' }
